0c6026efc54aa36178dd6db14ebcba2b2a8edeae
Leod123 <leo.dahl@scilifelab.se>
2025-05-22 11:17:50 +0200

    Make function for replacing defaults in a list

---
+++ R/replace_default.R
@@ -0,0 +1,25 @@
+#' Replace default elements in a named list with corresponding elements in a new list.
+#'
+#' @param default_param Named list with elements to potentially replace ("defaults").
+#' @param new_param Named list with elements to replace with.
+#'
+#' @returns A named list where overlapping elements with overlapping names are replaced.
+#' @export
+#'
+#' @examples
+#' replace_default(list(a = 1, b = 2, c = 3), list(a = 123, 456, d = 789))
+replace_default <- function(default_param, new_param) {
+  # Skip if no new parameters introduced
+  # if (length(new_param) == 0) return(NULL)
+
+  # Ignore unnamed elements
+  new_param <- new_param[names(new_param) != ""]
+
+  # Overlapping names
+  overlap_names <- intersect(names(default_param), names(new_param))
+
+  # Overwrite default values whose names exist in the specified parameters
+  default_param[overlap_names] <- new_param[overlap_names]
+
+  return(default_param)
+}
