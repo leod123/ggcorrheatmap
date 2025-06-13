5471c94c3434480d9169d88e074d79c6b8ebb411
Leod123 <leo.dahl@scilifelab.se>
2025-06-13 20:20:13 +0200

    Sketch on labelling cells with p-values

---
+++ R/test_cor.R
@@ -0,0 +1,65 @@
+
+test_cor <- function(x, y = NULL, full_plt = T, include_diag = F,
+                     method = "pearson", use = "everything", p_adj_method = "none") {
+  if (is.null(y) | identical(x, y)) {
+    test_pairs <- shape_mat_long(matrix(nrow = ncol(x), ncol = ncol(x), dimnames = list(colnames(x), colnames(x))), unique_pairs = T)
+
+    # Make a data frame of all relevant combinations to fill in with values later, to avoid running tests for duplicated pairs or diagonals
+    all_pairs <- if (full_plt) {
+      shape_mat_long(matrix(nrow = ncol(x), ncol = ncol(x), dimnames = list(colnames(x), colnames(x))))
+    } else {
+      test_pairs
+    }
+    all_pairs <- dplyr::select(all_pairs, -value)
+
+    # Get correlations and p-values only for half of the non-diagonal pairs
+    test_pairs <- dplyr::filter(test_pairs, as.character(row) != as.character(col))
+    test_pairs <- dplyr::bind_cols( # Bind together with cor, p, padj for each row
+      dplyr::select(test_pairs, -value), dplyr::bind_rows(
+        apply(test_pairs, 1, function(comb) {
+          tst <- cor.test(x[, comb["row"]], x[, comb["col"]])
+          data.frame(value = unname(tst$estimate), p_val = tst$p.value)
+        }, simplify = T)
+      )
+    )
+
+    # Adjust p-values
+    test_pairs[["p_adj"]] <- p.adjust(test_pairs[["p_val"]], method = p_adj_method)
+    # Fill the rest of the values
+    if (!full_plt & !include_diag) {
+      # If diagonal is not drawn, return only the non-diagonal values
+      return(test_pairs)
+
+    } else {
+      # Make column with ID of combination, join by ID to fill in other half of the matrix
+      test_pairs[["rowcol"]] <- apply(test_pairs, 1, function(i) paste(sort(c(i["row"], i["col"])), collapse = "_"))
+      all_pairs[["rowcol"]] <- apply(all_pairs, 1, function(i) paste(sort(c(i["row"], i["col"])), collapse = "_"))
+      all_pairs <- dplyr::left_join(all_pairs, dplyr::select(test_pairs, value, rowcol, p_val, p_adj), by = "rowcol")
+      all_pairs <- dplyr::select(all_pairs, -rowcol)
+
+      # Fill in diagonal values
+      all_pairs <- dplyr::mutate(all_pairs,
+                                 value = dplyr::case_when(row == col ~ 1, T ~ value),
+                                 p_val = dplyr::case_when(row == col ~ 0, T ~ p_val),
+                                 p_adj = dplyr::case_when(row == col ~ 0, T ~ p_adj))
+
+      return(all_pairs)
+    }
+
+  } else {
+    all_pairs <- shape_mat_long(matrix(nrow = ncol(x), ncol = ncol(y), dimnames = list(colnames(x), colnames(y))))
+    all_pairs <- dplyr::bind_cols( # Bind together with cor, p, padj for each row
+      dplyr::select(all_pairs, -value), dplyr::bind_rows(
+        apply(all_pairs, 1, function(comb) {
+          tst <- cor.test(x[, comb["row"]], y[, comb["col"]])
+          data.frame(value = unname(tst$estimate), p_val = tst$p.value)
+        }, simplify = T)
+      )
+    )
+
+    # Adjust p-values
+    all_pairs[["p_adj"]] <- p.adjust(all_pairs[["p_val"]], method = p_adj_method)
+
+    return(all_pairs)
+  }
+}
